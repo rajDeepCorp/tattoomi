7c03e7c87126332a794e2345d6f7e00c8d492dd8
RajDeep <rdcorpmail@gmail.com>
2026-06-11 16:12:46 +0530

    added firebase

---
+++ firebaseAdmin.ts
@@ -0,0 +1,20 @@
+import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const app =
+  getApps().length > 0
+    ? getApp()
+    : initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
+            /\\n/g,
+            "\n"
+          ),
+        }),
+      });
+
+export const adminAuth = getAuth(app);
+export const adminDb = getFirestore(app);
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQNZtP73yerM-ZYkjodpFHlDr8Vqf2TuY",
-  authDomain: "tattoomi-c7d53.firebaseapp.com",
-  databaseURL: "https://tattoomi-c7d53-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tattoomi-c7d53",
-  storageBucket: "tattoomi-c7d53.firebasestorage.app",
-  messagingSenderId: "623716115149",
-  appId: "1:623716115149:web:a709a4c042529c30b43f84",
-  measurementId: "G-LZG6ETZCG1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app =
+  getApps().length > 0
+    ? getApp()
+    : initializeApp(firebaseConfig);
