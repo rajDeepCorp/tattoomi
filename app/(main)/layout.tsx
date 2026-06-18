e4bf7bf21b58a584ba7b9481725225f93da29459
RajDeep <rdcorpmail@gmail.com>
2026-06-18 15:34:55 +0530

    fixed error of navbar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,6 +8,10 @@ import { headers } from "next/headers";
 export default async function MainLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     const session = await auth.api.getSession({ headers: await headers(), });
 
+    if (!session) {
+        return;
+      }
+
     return (
         <main className="relative shadow-inner shadow-stone-500 p-2 m-1 sm:rounded-2xl rounded-xl overflow-x-hidden">
             {children}
