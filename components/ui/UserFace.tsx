fad1e26a8c1ca44d96ee52a68fa2f02027b09fba
RajDeep <rdcorpmail@gmail.com>
2026-06-18 14:00:35 +0530

    circular facepic

--- components/ui/UserFace.tsx
+++ components/ui/UserFace.tsx
@@ -12,13 +12,15 @@ export const UserFace = ({ user }: UserFaceProps) => {
 
   return (
     <div className="relative sm:w-1/5 w-full shadow dark:shadow-inner shadow-stone-500 rounded-2xl mx-1 my-2 p-2 flex flex-col justify-start items-center gap-2">
-      <Image
-        width={150}
-        height={150}
-        src={user.image || "/userpic.jpg"}
-        alt="Profile Pic"
-        className="relative dark:shadow shadow-inner shadow-stone-500 rounded-full p-2 cursor-pointer"
-      />
+      <div className="relative w-37.5 h-37.5 rounded-full overflow-hidden dark:shadow shadow-inner shadow-stone-500 p-2">
+        <Image
+          src={user.image || "/userpic.jpg"}
+          alt="Profile Pic"
+          fill
+          className="rounded-full object-cover"
+          sizes="150px"
+        />
+      </div>
       <p className="relative dark:shadow min-w-1/2 shadow-inner shadow-stone-500 rounded-2xl py-0.5 text-xl text-center text-shadow-lg opacity-80 font-light">
         {user.name || "User Name"}
       </p>
